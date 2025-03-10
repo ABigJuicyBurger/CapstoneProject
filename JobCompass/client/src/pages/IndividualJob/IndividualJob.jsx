b7b1b3c3259b0fbbc22f5f25dd078f03adf1195b
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-10 16:03:51 -0600

    fetch all jobs from list

---
+++ JobCompass/client/src/pages/IndividualJob/IndividualJob.jsx
@@ -0,0 +1,12 @@
+import JobCard from "../../components/JobCard/JobCard";
+
+function IndividualJob() {
+  return (
+    <>
+      <h1>This is the individual job page</h1>
+      <JobCard />
+    </>
+  );
+}
+
+export default IndividualJob;
