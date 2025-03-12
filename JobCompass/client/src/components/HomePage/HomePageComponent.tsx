cd3b356cdd267dfd96fcc38a887f4ff43ea097d4
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-12 16:13:41 -0600

    clean up homepage component and type it

---
+++ JobCompass/client/src/components/HomePage/HomePageComponent.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { JSX } from "react";
+
+import "./HomePageComponent.scss";
+
+function HomePage(): JSX.Element {
+  return (
+    <div className="homePage">
+      <header className="homePage__header">
+        <Link className="homePage__header__signIn-cta" to={"/signIn"}>
+          Sign In
+        </Link>
+        <Link className="homePage__header__register-cta" to={"/register"}>
+          Register
+        </Link>
+      </header>
+      <main className="heroPage">
+        <h1 className="heroPage__title">JobCompass</h1>
+        <h2 className="heroPage__text">Let's Help You Find Your Next Job</h2>
+        <Link className="heroPage__guest-cta" to={"/jobs"}>
+          Try Our Page Out!
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
