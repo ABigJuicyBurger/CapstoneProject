0447c0885e1f24afc4bd54da2878c61379d8edd8
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-12 18:34:14 -0600

    successfully show each job based on advanced marker function that google api provides

--- JobCompass/client/src/components/JobList/JobList.tsx
+++ JobCompass/client/src/components/JobList/JobList.tsx
@@ -6,25 +6,8 @@ import "./JobList.scss";
 import JobCardType from "../../../types/JobCardType";
 import { JSX } from "react/jsx-runtime"; // needed to find JSX namespace for TS
 
-function JobList(): JSX.Element {
+function JobList({ jobBoard }): JSX.Element {
   const backendURL = import.meta.env.VITE_BACKEND_URL;
-  const [jobBoard, setJobBoard] = useState<JobCardType[] | null>([]); // expecting array of jobs similar to JobCardType
-
-  const fetchAllJobs = async (): Promise<void | JSX.Element> => {
-    try {
-      const jobListResponse = await axios.get(`${backendURL}/jobs`);
-      console.log(jobListResponse.data);
-      setJobBoard(jobListResponse.data);
-    } catch (error: any) {
-      console.log(error.message);
-      setJobBoard(null);
-    }
-  };
-
-  // load all jobs
-  useEffect(() => {
-    fetchAllJobs();
-  }, []);
 
   // runtime check, doesn't need a type annotation (no type declared)
   if (!jobBoard) {
