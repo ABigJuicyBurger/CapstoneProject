f7af00ccf4585383e19a48f0531e8fcb50c17e5d
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-15 15:16:05 -0600

    separate header into is own component

---
+++ JobCompass/client/src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header className="homePage__header">
+      <div className="homePage__header__logo">
+        <Link to={"/"}>
+          <img
+            className="homePage__header__logo__image"
+            src="/src/assets/Logo/compassfavicon.png"
+            alt=""
+          />
+
+          <h1>JobCompass</h1>
+        </Link>
+      </div>
+      <Link className="homePage__header__signIn-cta" to={"/signIn"}>
+        Sign In
+      </Link>
+      <Link className="homePage__header__register-cta" to={"/register"}>
+        Register
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
