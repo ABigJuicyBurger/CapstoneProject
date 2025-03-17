6d85c490e48f741fd557fa2c4901ca75818981fa
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-17 16:05:35 -0600

    style the header to prevent vertical overflow

--- JobCompass/client/src/components/HomePage/HomePageComponent.tsx
+++ JobCompass/client/src/components/HomePage/HomePageComponent.tsx
@@ -6,7 +6,6 @@ import "./HomePageComponent.scss";
 function HomePage(): JSX.Element {
   return (
     <div className="homePage">
-      {/* <Header /> */}
       <main className="heroPage">
         <h1 className="heroPage__title">JobCompass</h1>
         <h2 className="heroPage__text">Let's Help You Find Your Next Job</h2>
