891b9f5b1f94f8f76441c9eddd52bd0fbd3e4d77
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-03-17 17:44:50 -0600

    show notepad on a switch state with button click event

--- JobCompass/client/src/components/JobNote/JobNote.tsx
+++ JobCompass/client/src/components/JobNote/JobNote.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { JSX } from "react";
 import MapJobCardNoteType from "../../../types/MapJobCardType";
 
-import AddNote from "./AddNote.js";
+import AddNote from "./AddNote.tsx";
 
 function JobNote({ noteState, updateNote }: MapJobCardNoteType): JSX.Element {
   const { id } = useParams();
@@ -11,6 +11,8 @@ function JobNote({ noteState, updateNote }: MapJobCardNoteType): JSX.Element {
 
   // TODO: note state; for now keep it guest status (where user has no saveable notes)
   const [noteList, setNoteList] = useState([]);
+
+  const addNote = () => {};
 
   return (
     <div className="jobCard">
@@ -24,12 +26,6 @@ function JobNote({ noteState, updateNote }: MapJobCardNoteType): JSX.Element {
         </Link>
         <h2 className="jobCard__header__title">Notes</h2>
         <section className="jobCard__header__title__company">
-          {/* <h3 className="jobCard__header__company">{job.company}</h3>
-                  <img
-                    src="/  "
-                    className="jobCard__header__logo-placeholder"
-                    alt="logo"
-                  /> */}
           {noteList.length > 0 ? (
             <div>
               <AddNote />
@@ -42,10 +38,7 @@ function JobNote({ noteState, updateNote }: MapJobCardNoteType): JSX.Element {
             </div>
           )}
         </section>
-        <div className="jobCard__header__cta">
-          {/* <button onClick={saveJob}> Save job </button> */}
-          {/* <button onClick={() => addNote}> Add Note </button> */}
-        </div>
+        {/* <div className="jobCard__header__cta"></div> */}
       </div>
     </div>
   );
