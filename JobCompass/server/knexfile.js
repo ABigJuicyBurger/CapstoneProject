4d37fd3c8fb3cdec111190c41ecfd1b90b607e3b
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-04-29 16:29:37 -0600

    adjust knexfile and env to appeal to render requirements for netlify/render combo

--- JobCompass/server/knexfile.js
+++ JobCompass/server/knexfile.js
@@ -1,11 +1,41 @@
 import "dotenv/config";
+
 export default {
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8mb4",
+  development: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      database: process.env.DB_NAME || "jobcompass",
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres",
+      charset: "utf8",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 };
